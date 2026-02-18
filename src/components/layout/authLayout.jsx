486571a930c6172ff0f70d4ab9170ca0c5806f0c
Raza Aziz <razaaziz9191@gmail.com>
2026-02-18 18:09:46 +0500

    Changed image and font

--- src/components/layout/authLayout.jsx
+++ src/components/layout/authLayout.jsx
@@ -2,32 +2,34 @@ import React from "react";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="grid min-h-svh lg:grid-cols-2">
+    <div className="grid min-h-svh lg:grid-cols-[0.415fr_0.585fr]">
       {/* LEFT SIDE: The Branding & Image (Hidden on mobile) */}
 
       <div className="bg-muted relative hidden lg:block flex-col justify-between p-12 text-white">
         <img
-          src="https://unsplash.com/photos/gray-padded-chaise-couch-beside-window-rEJxpBskj3Q"
+          src="https://images.unsplash.com/photo-1564078516393-cf04bd966897?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="Image"
-          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="absolute inset-0 h-full w-full object-cover brightness-75 dark:brightness-[0.2] dark:grayscale"
         />
 
         {/* Brand Logo - Top Left */}
-        <div className="relative z-10">
-          <h2 className="text-2xl font-serif font-semibold tracking-tight">
+        <div className="absolute top-8 left-8 z-10">
+          <span className="text-2xl font-[Playfair_Display] font-semibold text-white tracking-tight">
             Boundry.
-          </h2>
+          </span>
         </div>
 
         {/* Quote Section - Bottom Left */}
-        <div className="relative z-10 max-w-md">
+        <div className="absolute bottom-12 left-12 z-10 max-w-md">
           <blockquote className="space-y-4">
-            <p className="text-3xl font-serif italic leading-tight">
+            <h2 className="text-4xl font-[Playfair_Display] mb-4 italic">
               &ldquo;Luxury must be comfortable, otherwise it is not
               luxury.&rdquo;
-            </p>
+            </h2>
             <footer className="flex flex-col gap-2">
-              <span className="text-lg">— Coco Chanel</span>
+              <span className="text-sm opacity-80 font-[Poppins]">
+                — Coco Chanel
+              </span>
               {/* Pagination Dots Indicator */}
               <div className="flex gap-2 mt-4">
                 <div className="h-1 w-8 bg-white rounded-full" />
