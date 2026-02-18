9e86784b53ad2e6834bb20ada5f8a7ab24d47a1f
Raza Aziz <razaaziz9191@gmail.com>
2026-02-18 20:26:01 +0500

    Added toaster globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Outlet } from "react-router-dom";
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click Me</Button>
-    </div>
-  )
+    <>
+      <Toaster richColors position="top-right" />
+
+      <Outlet>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+          <Button>Click Me</Button>
+        </div>
+      </Outlet>
+    </>
+  );
 }
 
-export default App
+export default App;
