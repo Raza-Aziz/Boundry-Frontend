39b852d1fb75c027a46070ca8e1ae4db1b7bd853
Raza Aziz <razaaziz9191@gmail.com>
2026-02-19 22:04:42 +0500

    Built a simple spinner to check if the user is authenticated or not

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { Button } from "@/components/ui/button";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
+import { useGetCurrentUserQuery } from "./store/api/authApi";
 
 function App() {
+  const { isLoading } = useGetCurrentUserQuery();
+
+  if (isLoading) {
+    return (
+      <div className="h-screen w-full flex items-center justify-center bg-[#F9F8F3]">
+        <span className="font-serif text-2xl animate-pulse text-[#A3634B]">
+          Boundry
+        </span>
+      </div>
+    );
+  }
+
   return (
     <>
       <Toaster richColors position="top-right" />
