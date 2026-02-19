41a1d4d3e327425baa9722c00005be69cf75ec91
Raza Aziz <razaaziz9191@gmail.com>
2026-02-19 22:10:12 +0500

    Added auth reducer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { baseApi } from "./api/baseApi";
+import authReducer from "./slices/authSlice";
 
 const store = configureStore({
   // we add reducers in it
   reducer: {
     // ? : What does this line do?
     [baseApi.reducerPath]: baseApi.reducer,
+    auth: authReducer,
   },
   // Adding the api middleware enables caching, invalidation, and polling
   middleware: (getDefaultMiddleware) =>
