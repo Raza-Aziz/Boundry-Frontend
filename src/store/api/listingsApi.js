cbbd6db00150a34bdd757d8ac78fdcd0075d4ed4
Raza Aziz <razaaziz9191@gmail.com>
2026-02-25 22:41:33 +0500

    Added single listing and similar listings queries

--- src/store/api/listingsApi.js
+++ src/store/api/listingsApi.js
@@ -9,9 +9,27 @@ export const listingsApi = baseApi.injectEndpoints({
         // RTK Query auto handles URL params
         params,
       }),
-      invalidatesTags: ["Listings"],
+      providesTags: ["Listings"],
+    }),
+
+    getListing: builder.query({
+      query: (id) => ({
+        url: `/listings/${id}`, // Clean URL
+        method: "GET",
+      }),
+      providesTags: (result, error, id) => [{ type: "Listings", id }],
+    }),
+
+    getSimilarListings: builder.query({
+      query: ({ city, status }) => ({
+        url: `/listings?city=${city}&status=${status}&limit=3&sort=-createdAt`,
+      }),
     }),
   }),
 });
 
-export const { useGetListingsQuery } = listingsApi;
+export const {
+  useGetListingsQuery,
+  useGetListingQuery,
+  useGetSimilarListingsQuery,
+} = listingsApi;
